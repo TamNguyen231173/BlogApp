05c30d2b717c1cfb227e5f2c486e02c189205cac
TamNguyen <tamnguyen@gmail.com>
2023-01-27 12:39:12 +0700

    set up theme

---
+++ src/infrastructure/theme/colors.js
@@ -0,0 +1,22 @@
+export const colors = {
+	ui: {
+		primary: "#1877F2",
+		disabled: "#EEF1F4",
+		error: "#ED2E7E",
+		success: "#00BA88",
+		warning: "#F4B740",
+	},
+	bg: {
+		primary: "#FFFFFF",
+		secondary: "#1C1E21",
+		body_dark: "#B0B3B8",
+		input_dark: "#3A3B3C",
+	},
+	text: {
+		primary: "#4E4B66",
+		title_active: "#050505",
+		button_text: "#667080",
+		placeholder: "#A0A3BD",
+		title_dark: "#E4E6EB",
+	},
+};
---
+++ src/infrastructure/theme/fonts.js
@@ -0,0 +1,23 @@
+export const fonts = {
+	body: "Poppins_400Regular",
+	heading: "Poppins_700Regular",
+	monospace: "Poppins_400Regular",
+};
+
+export const fontWeights = {
+	regular: 400,
+	medium: 500,
+	bold: 700,
+};
+
+export const fontSizes = {
+	caption: "14px",
+	button: "16px",
+	body: "16px",
+	title: "32px",
+	h5: "24px",
+	h4: "34px",
+	h3: "45px",
+	h2: "56px",
+	h1: "112px",
+};
